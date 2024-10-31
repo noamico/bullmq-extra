b3a6311106abd70e15f2202d29a5684193e197b7
org <org@everc.com>
2024-10-31 21:07:42 +0200

    feat: coded reqrep

--- src/reqrep/reqrep.test.ts
+++ src/reqrep/reqrep.test.ts
@@ -23,18 +23,24 @@ describe('reqrep', function () {
     });
   });
 
-  describe('when requesting', () => {
-    it('should reply', async () => {
-      const requesterName = `test-${v4()}`;
+  describe('when requesting from multiple requesters', () => {
+    it('should reply separately to each', async () => {
+      const requesterName1 = `test-${v4()}`;
+      const requesterName2 = `test-${v4()}`;
       const responderName = `test-${v4()}`;
 
       const responder = new Responder({
         opts: { connection },
         responderName,
       });
-      const requester = new Requester({
+      const requester1 = new Requester({
         opts: { connection },
-        requesterName,
+        requesterName: requesterName1,
+        responderName,
+      });
+      const requester2 = new Requester({
+        opts: { connection },
+        requesterName: requesterName2,
         responderName,
       });
 
@@ -43,23 +49,35 @@ describe('reqrep', function () {
         return { value: value + 1 };
       });
 
-      const responses: { value: number }[] = [];
+      const responses1: { value: number }[] = [];
+      const responses2: { value: number }[] = [];
 
-      requester.processResponses(async (data) => {
-        responses.push(data);
+      requester1.processResponses(async (data) => {
+        responses1.push(data);
+      });
+
+      requester2.processResponses(async (data) => {
+        responses2.push(data);
       });
 
       for (let i = 0; i < 10; i++) {
-        await requester.request({ value: i });
+        await requester1.request({ value: i });
+        await requester2.request({ value: i * 20 });
       }
 
-      while (responses.length < 10) {
+      while (responses1.length < 10 || responses2.length < 10) {
         await delay(50);
       }
 
-      expect(responses).toEqual(
+      expect(responses1).toEqual(
         Array.from(Array(10).keys()).map((i) => ({
           value: i + 1,
+        })),
+      );
+
+      expect(responses2).toEqual(
+        Array.from(Array(10).keys()).map((i) => ({
+          value: i * 20 + 1,
         })),
       );
     });
